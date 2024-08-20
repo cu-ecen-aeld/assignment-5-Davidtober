b5d82ea08f2fe622dc26ba712ffe91088deee024
David Tober <david@sweatr.com>
2024-08-20 17:48:31 +1000

    Added LDD package to config file

--- base_external/package/ldd/symLinkFiles.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mkdir "$1/etc/rc.3"
-mkdir "$1/etc/rc.5"
-ln -sf "$1/etc/init.d/S981ddmodules" "$1/etc/rc.3/S981ddmodules"
-ln -sf "$1/etc/init.d/S981ddmodules" "$1/etc/rc.5/S981ddmodules"
